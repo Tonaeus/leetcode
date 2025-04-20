673741b66ad90fa1e6c2d4f21bad2b5dba1a3ad7
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-04-20 16:58:12 -0600

    solved #3523 - Make Array Non-decreasing

---
+++ solved/3523-make-array-non-decreasing.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int maximumPossibleSize(vector<int>& nums) {
+        int ans = 1;
+        int maxNum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] >= maxNum) {
+                ans++;
+                maxNum = nums[i];
+            }
+        }
+
+        return ans;
+    }
+};
