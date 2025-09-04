f405aa13cb1844845fae04cdaf6e8ee6e737f64e
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-09-04 15:44:15 -0600

    solved #3658 - GCD of Odd and Even Sums

---
+++ solved/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
