3df3a9e7740492cf5e7a180bbe18c23414763f63
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2025-09-05 18:25:34 -0600

    solved #3659 - Partition Array Into K-Distinct Groups

---
+++ solved/3659-partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        if (n % k != 0) {
+            return false;
+        }
+
+        unordered_map<int, int> map;
+        for (int num : nums) {
+            map[num]++;
+        }
+
+        int g = n / k;
+        for (const auto& pair : map) {
+            if (pair.second > g) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
