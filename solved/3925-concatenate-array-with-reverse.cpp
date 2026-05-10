6da83f300441659b723bd0976a21e43065fd2306
Tony Tran <113302511+Tonaeus@users.noreply.github.com>
2026-05-10 14:23:16 -0600

    solved #3925 - Concatenate Array With Reverse

---
+++ solved/3925-concatenate-array-with-reverse.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> concatWithReverse(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = n - 1; i >= 0; i--) {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
